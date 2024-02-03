f1cfb05faad2318a7511b853cb534064d8b42b29
Allison Serrano <allisonserrano171@yahoo.com>
2024-02-03 15:28:41 -0500

    cleaned up commented code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Animals } = require('../../models');
+const { User } = require('../../models');
 
  router.post('/', async (req, res) => {
    try {
